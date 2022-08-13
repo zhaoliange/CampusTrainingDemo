e9475733987e7c9025bb829b970892d292224629
Liang Zhao <liang.e.zhao@avanade.com>
2022-08-13 18:35:34 +0800

    Add Model Validation demo.

---
+++ ModelValidationDemo/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+using ModelValidationDemo.Models;
+using System.Diagnostics;
+
+namespace ModelValidationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult BodySample([FromBody] UserModel model)
+
+        {
+
+            // 由于 Id 是 int 类型，int 默认为 0
+
+            // 虽然带上了 [Required]，但不是 null 所以算是有值。
+
+            if (model.Id < 1)
+
+            {
+
+                ModelState.AddModelError("Id", "Id not exist");
+
+            }
+
+            if (ModelState.IsValid)
+
+            {
+
+                return Ok(model);
+
+            }
+
+            return BadRequest(ModelState);
+
+        }
+    }
+}
---
+++ ModelValidationDemo/Models/UserModel.cs
@@ -0,0 +1,41 @@
+﻿using ModelValidationDemo.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingDemo.Models
+{
+    public class UserModel
+    {
+        [Required]
+
+        public int Id { get; set; }
+
+
+
+        [RegularExpression(@"\w+")]
+
+        [StringLength(20, MinimumLength = 4)]
+
+        public string Name { get; set; }
+
+
+
+        [EmailAddress]
+
+        public string Email { get; set; }
+
+
+
+        [Phone]
+
+        public string PhoneNumber { get; set; }
+
+
+
+        [StringLength(200)]
+
+        public string Address { get; set; }
+
+        [AgeCheck(0,100)]
+        public DateTime BirthDate { get; set; }
+    }
+}

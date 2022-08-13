9b5f0dde08bed05ba41de9651b93d22458f3ab37
Liang Zhao <liang.e.zhao@avanade.com>
2022-08-13 23:42:14 +0800

    Add state management demo.

---
+++ StateManagementDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+var app = builder.Build();
+app.UseSession();
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (context) =>
+{
+    string message;
+    if (!context.Request.Cookies.TryGetValue("Sample", out message))
+    {
+        message = "Save data to cookies.";
+    }
+    context.Response.Cookies.Append("Sample", "This is Cookies.");
+    // 刪除 Cookies 数据
+    //context.Response.Cookies.Delete("Sample");
+    await context.Response.WriteAsync($"{message}");
+
+});
+
+app.Run(async (context) =>
+{
+    context.Session.SetString("Sample", "This is Session.");
+    string message = context.Session.GetString("Sample");
+    await context.Response.WriteAsync($"{message}");
+});
+
+app.Run();

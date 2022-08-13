04cd1a268c33103470ba7ff0feddd0d96fce111c
Liang Zhao <liang.e.zhao@avanade.com>
2022-08-13 16:46:29 +0800

    Add model binding demo.

---
+++ ModelBindingDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+using System.Diagnostics;
+
+namespace ModelBindingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult FirstSample(
+
+            [FromHeader] string header,
+
+            [FromForm] string form,
+
+            [FromRoute] string id,
+
+            [FromQuery] string query)
+
+        {
+
+            return Content($"header: {header}, form: {form}, id: {id}, query: {query}");
+
+        }
+
+
+        public IActionResult DISample([FromServices] ILogger<HomeController> logger)
+
+        {
+
+            return Content($"logger is null: {logger == null}.");
+
+        }
+
+
+
+        public IActionResult BodySample([FromBody] UserModel model)
+
+        {
+
+            return Ok(model);
+
+        }
+    }
+}

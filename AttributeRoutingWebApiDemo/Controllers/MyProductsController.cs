92918ce2192179d0fa3960190d868de00b05daff
Liang Zhao <liang.e.zhao@avanade.com>
2022-08-16 01:14:50 +0800

    Add changes.

---
+++ AttributeRoutingWebApiDemo/Controllers/MyProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace AttributeRoutingWebApiDemo.Controllers
+{
+    [ApiController]
+    public class MyProductsController : ControllerBase
+    {
+        [HttpGet("/products3")]
+        public IActionResult ListProducts()
+        {
+            return ControllerContext.MyDisplayRouteInfo();
+        }
+
+        [HttpPost("/products3")]
+        public IActionResult CreateProduct(MyProduct myProduct)
+        {
+            return ControllerContext.MyDisplayRouteInfo(myProduct.Name);
+        }
+    }
+}
---
+++ AttributeRoutingWebApiDemo/MyProduct.cs
@@ -0,0 +1,7 @@
+﻿namespace AttributeRoutingWebApiDemo
+{
+    public class MyProduct
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}

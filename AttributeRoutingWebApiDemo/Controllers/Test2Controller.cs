0d29839f2d999cbc04e8dc36fcd60a26bae939e7
Liang Zhao <liang.e.zhao@avanade.com>
2022-08-16 00:43:24 +0800

    Add AttributeRoutingWebApiDemo

---
+++ AttributeRoutingWebApiDemo/Controllers/Test2Controller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace AttributeRoutingWebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test2Controller : ControllerBase
+    {
+        [HttpGet]   // GET /api/test2
+        public IActionResult ListProducts()
+        {
+            return ControllerContext.MyDisplayRouteInfo();
+        }
+
+        [HttpGet("{id}")]   // GET /api/test2/xyz
+        public IActionResult GetProduct(string id)
+        {
+            return ControllerContext.MyDisplayRouteInfo(id);
+        }
+
+        [HttpGet("int/{id:int}")] // GET /api/test2/int/3
+        public IActionResult GetIntProduct(int id)
+        {
+            return ControllerContext.MyDisplayRouteInfo(id);
+        }
+
+        [HttpGet("int2/{id}")]  // GET /api/test2/int2/3
+        public IActionResult GetInt2Product(int id)
+        {
+            return ControllerContext.MyDisplayRouteInfo(id);
+        }
+    }
+}

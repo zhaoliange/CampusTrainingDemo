b4a791b0e6d64fc037b09365b5d4e1aa9665e332
Liang Zhao <liang.e.zhao@avanade.com>
2022-08-16 08:32:00 +0800

    Add Changes.

---
+++ AttributeRoutingWebApiDemo/Controllers/Products2ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace AttributeRoutingWebApiDemo.Controllers
+{
+    [ApiController]
+    public class Products2ApiController : ControllerBase
+    {
+        [HttpGet("/products2/{id}", Name = "Products_List")]
+        public IActionResult GetProduct(int id)
+        {
+            return ControllerContext.MyDisplayRouteInfo(id);
+        }
+    }
+}
---
+++ AttributeRoutingWebApiDemo/Controllers/ProductsApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace AttributeRoutingWebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductsApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ListProducts()
+        {
+            return ControllerContext.MyDisplayRouteInfo();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return ControllerContext.MyDisplayRouteInfo(id);
+        }
+    }
+}
